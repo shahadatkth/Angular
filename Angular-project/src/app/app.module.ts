a2e832f098d69341121cbb27611f3b61afe64669
shahadatkth <russel1433@gmail.com>
2019-05-21 10:40:15 +0200

    added angular user registrat

---
+++ Angular-project/src/app/app.module.ts
@@ -0,0 +1,51 @@
+﻿import { NgModule }      from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MustMatchDirective } from './_helpers/must-match.directive';
+
+
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_components';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        routing,
+        CommonModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        MustMatchDirective
+
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ Angular-project/src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+﻿import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
+
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit, OnDestroy {
+    currentUser: User;
+    currentUserSubscription: Subscription;
+    user: object;
+    editing: boolean = false;
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+            this.currentUser = user;
+        });
+    }
+
+
+    ngOnInit() {
+      console.log(this.currentUser.id);
+      this.loadUser();
+    }
+
+    ngOnDestroy() {
+        // unsubscribe to ensure no memory leaks
+        this.currentUserSubscription.unsubscribe();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(this.currentUser.id).pipe(first()).subscribe(() => {
+
+        });
+    }
+    updateUser(currentUser: User) {
+        console.log(currentUser);
+        this.userService.update(this.currentUser).pipe(first()).subscribe((user) => {
+            this.user = user;
+        });
+    }
+
+    private loadUser() {
+        this.userService.getById(this.currentUser.id).pipe(first()).subscribe((user) => {
+            this.user = Object.assign({}, user);
+           /* this.user = user;*/
+
+
+        });
+    }
+
+}
